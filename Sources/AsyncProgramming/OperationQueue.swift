ba04410697b52f9cf444433832e1f2635818ec3c
ShengHua Wu <fantasy0404@gmail.com>
2022-07-05 13:12:51 +0200

    Operation problems.

--- Sources/AsyncProgramming/OperationQueue.swift
+++ Sources/AsyncProgramming/OperationQueue.swift
@@ -1,5 +1,15 @@
 import Foundation
 
+// Conclusions:
+//
+// 1. Operation does not provide async non-blocking
+//
+// 2. Operation coordination does not work well with cancellation
+//
+// 3. Operation API is heavily OOP design
+//
+// 4. Operation does not avoid multithread race condition
+
 func operationQueueBasics() {
     let queue = OperationQueue()
     
@@ -69,4 +79,25 @@ func operationCoordination() {
     queue.addOperation(operationB)
     queue.addOperation(operationC)
     queue.addOperation(operationD)
+    
+    // B, C, D will keep running even though A is cancelled
+    operationA.cancel()
+}
+
+func operationAsynchronousBlocking() {
+    let queue = OperationQueue()
+    
+    // This will block all threads
+    for n in 0 ..< workCount {
+        queue.addOperation {
+            print(n, Thread.current)
+            while true {}
+        }
+    }
+    
+    // This won't be executed at all
+    queue.addOperation {
+        print("Starting prime operation")
+        nthPrime(50_000)
+    }
 }
