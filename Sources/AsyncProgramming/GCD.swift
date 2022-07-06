d27625f9f5cfc9d62351ccca642d07496cd967f4
ShengHua Wu <fantasy0404@gmail.com>
2022-07-06 15:29:38 +0200

    gcd priority and cancellation.

--- Sources/AsyncProgramming/GCD.swift
+++ Sources/AsyncProgramming/GCD.swift
@@ -16,11 +16,40 @@ func gcdNonblocking() {
     // all without blocking the current thread.
     // The scheduling happens at a deeper level with the OS
     // so that we don’t need to waste time on the thread waiting for time to pass.
-    let queue = DispatchQueue(label: "basics")
+    let queue = DispatchQueue(label: "non-blocking")
     
     print("before scheduling")
     queue.asyncAfter(deadline: .now() + 1) {
         print("1 second passed")
     }
     print("after scheduling")
+}
+
+func gcdPriorityAndCancellation() {
+    // Set the priority as `qos`
+    let queue = DispatchQueue(label: "priority-and-cancellation", qos: .background)
+    
+    var item: DispatchWorkItem! // This is not a good practice
+    item = DispatchWorkItem {
+        defer { item = nil }
+        
+        let start = Date()
+        defer { print("Finished in", Date().timeIntervalSince(start)) }
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        // Have to check this manually
+        // Otherwise, the code will continue even if the item is cancelled
+        // This cancellation process is cooperative
+        guard !item.isCancelled else {
+            print("Cancelled!")
+            return
+        }
+        print(Thread.current)
+    }
+    
+    queue.async(execute: item)
+    
+    Thread.sleep(forTimeInterval: 0.5)
+    item.cancel()
 }
