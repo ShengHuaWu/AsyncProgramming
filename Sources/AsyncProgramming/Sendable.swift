0c18015e57d45382b3dba99cb9c58975ecb4b44c
ShengHua Wu <fantasy0404@gmail.com>
2022-07-21 10:17:16 +0200

    at sendable.

--- Sources/AsyncProgramming/Sendable.swift
+++ Sources/AsyncProgramming/Sendable.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 func sendableBasics() {
-    // The Sendable protocol indicates that value of the given type can
-    // be safely used in concurrent code.
+    // Sendable protocol allows us to prove to the compiler
+    // that values of a specific type are safe to be passed across concurrent boundaries
     //
     // We personally feel quite confident this type is safe to use from multiple threads,
     // and so we can tell the compiler to just trust us
@@ -27,4 +27,37 @@ func sendableBasics() {
 
     Thread.sleep(forTimeInterval: 2)
     print("counter.count", counter.count)
+}
+
+struct User {}
+
+// @Sendable attribute allows us to prove to the compiler
+// that functions can be safely used from multiple concurrent contexts.
+struct DatabaseClient {
+    
+    // You can also use @Sendable to help make types
+    // that hold onto closures conform to the Sendable protocol.
+    var fetchUsers: @Sendable () async throws -> [User]
+    var createUser: @Sendable (User) async throws -> Void
+}
+
+extension DatabaseClient {
+    static let live = Self(
+        fetchUsers: { fatalError() },
+        createUser: { _ in fatalError() }
+    )
+}
+
+func atSendable() {
+    func perform(client: DatabaseClient, work: @escaping @Sendable () -> Void) {
+        // If you can prove to the compiler that your closure is @Sendable,
+        // then the function you are invoking can be free to use
+        // that closure in any concurrent way it wants without leading to a race condition
+        Task {
+            _ = try await client.fetchUsers()
+        }
+        Task {
+            _ = try await client.fetchUsers()
+        }
+    }
 }
